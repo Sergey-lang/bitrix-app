f0734f439bc35d0f46994754ef2665c421cab517
Sergey <sergey@Sergeys-MacBook-Air.local>
2026-09-10 14:27:18 +0300

    feat: add local Bitrix learning environment

---
+++ public/.section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная";
+$arDirProperties = array(
+   "description" => "Учебный портал по 1С-Битрикс: шаблоны, компоненты, инфоблоки, роли, API и кеширование.",
+   "keywords" => "1С-Битрикс, обучение, PHP, компоненты, инфоблоки, API",
+   "robots" => "index, follow"
+);
+?>
